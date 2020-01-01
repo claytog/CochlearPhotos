aba3d2ef24c9375f1064c676487d346051d4307c
Clayton Gilbert <claytongilbert@gmail.com>
2020-01-01 21:39:59 +1100

    Build 0.21

--- CochlearPhotos/ViewControllers/LocViewController.swift
+++ CochlearPhotos/ViewControllers/LocViewController.swift
@@ -20,7 +20,8 @@ class LocViewController: UIViewController {
     let locationDetailSegue = "locationDetailSegue"
     let LOADED = "loaded"
     
-    let mapGap: CGFloat = 120
+    let centreOffset: CGFloat = 50
+    let mapGapTop: CGFloat = 120
     var mapGapBottom: CGFloat!
     var mapViewHeight: CGFloat!
     var locViewOriginalCenter: CGPoint!
@@ -37,30 +38,23 @@ class LocViewController: UIViewController {
         super.viewDidLoad()
                 
         setupView()
-        
         initLocTable()
-        
         setCurrentLocation()
-        
         mapView.delegate = self
         // Do any additional setup after loading the view.
         if DBManager.shared.openOrCopyDatabase() { // initialise a singleton instance of the database
             
             LocationsAPI.get( completion: { locationsResponse in
-                                  //   self.weatherResponse = weatherResponse
-            print ("\n**** CONTENT DONLOADED, opening storyboard ***\n")
-            self.setupLocTableView()
-                
+                print ("\n**** CONTENT DONLOADED, opening storyboard ***\n")
+                self.setupLocTableView()
             })
         }
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        
-        locTableView.alpha = 1
         locationManager.requestWhenInUseAuthorization()
-        
+        setHeights()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -69,23 +63,20 @@ class LocViewController: UIViewController {
     }
    
     override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
-           
         setupLocTableView()
         setHeights()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-           
-           if segue.identifier == locationDetailSegue {
-               if let locDetailViewController = segue.destination as? LocDetailViewController {
-                   locDetailViewController.selectedLocation = sender as? Location
-               }
+       if segue.identifier == locationDetailSegue {
+           if let locDetailViewController = segue.destination as? LocDetailViewController {
+               locDetailViewController.selectedLocation = sender as? Location
            }
        }
+    }
     
     func setupView(){
         pressedLoc = LOADED
-        
         NotificationCenter.default.addObserver(self, selector: #selector(willEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
     }
     
@@ -96,12 +87,10 @@ class LocViewController: UIViewController {
     }
     
     @objc func willEnterForeground() {
-
         setupLocTableView()
     }
     
     func initLocTable(){
-        
         locTableView.delegate = self
         locTableView.dataSource = self
         locTableView.tableFooterView = UIView()
@@ -109,7 +98,6 @@ class LocViewController: UIViewController {
     }
     
     func annotateLocationList(completion : @escaping (Bool)->()){
-        
         for annotation in mapView.annotations{
             mapView.removeAnnotation(annotation)
         }
@@ -131,25 +119,22 @@ class LocViewController: UIViewController {
     }
     
     @IBAction func didPanMapListView(_ sender: UIPanGestureRecognizer) {
-        
         let translation = sender.translation(in: view)
 
         if sender.state == UIGestureRecognizer.State.began {
             locViewOriginalCenter = locationsView.center
             locViewOriginalY = locationsView.frame.origin.y
-   
         } else if sender.state == UIGestureRecognizer.State.changed {
             let newHeight = locListHeightOrig - translation.y
             mapListHeightConstraint.constant = newHeight
-          
         } else if sender.state == UIGestureRecognizer.State.ended {
             
             locListHeightOrig = mapListHeightConstraint.constant
             
-            if locationsView.frame.origin.y < mapGap {
+            if locationsView.frame.origin.y < mapGapTop {
                 UIView.animate(withDuration:0.6, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
                 animations: { () -> Void in
-                    self.locationsView.frame.origin.y = self.mapGap
+                    self.locationsView.frame.origin.y = self.mapGapTop
                     self.mapListHeightConstraint.constant = self.locListHeightDef
                     self.locListHeightOrig = self.locListHeightDef
                 }, completion: nil)
@@ -157,22 +142,20 @@ class LocViewController: UIViewController {
             }else if (locationsView.frame.origin.y > mapGapBottom ) {
                 UIView.animate(withDuration:0.6, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
                 animations: { () -> Void in
-                    self.locationsView.frame.origin.y = self.mapGapBottom + self.mapGap
-                    self.mapListHeightConstraint.constant = self.mapGap
-                    self.locListHeightOrig = self.mapGap
+                    self.locationsView.frame.origin.y = self.mapGapBottom + self.mapGapTop
+                    self.mapListHeightConstraint.constant = self.mapGapTop
+                    self.locListHeightOrig = self.mapGapTop
                 }, completion: nil)
             }
         }
     }
     
     @IBAction func didLongPressMapView(_ sender: UILongPressGestureRecognizer) {
-
         let touchPoint = sender.location(in: mapView)
         let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
         let annotation = MKPointAnnotation()
         annotation.coordinate = newCoordinates
         mapView.addAnnotation(annotation)
-        
         
         let loc = Location(locType: LocType.custom, coordinate: annotation.coordinate)
         selectedLoc = loc.toCL()
@@ -206,18 +189,15 @@ extension LocViewController: MKMapViewDelegate {
             dequeuedView.annotation = annotation
             view = dequeuedView
         } else {
-            
             view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
             view.canShowCallout = false
-            view.calloutOffset = CGPoint(x: -5, y: 5)
-            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-             
+       //     view.calloutOffset = CGPoint(x: -5, y: 5)
+       //     view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
         }
         return view
     }
  
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-        
         let loc:Location!
         let name = view.annotation?.title
         
@@ -238,22 +218,18 @@ extension LocViewController: MKMapViewDelegate {
 extension LocViewController:  UITableViewDataSource, UITableViewDelegate {
     
     func setupLocTableView() {
-        
-        locListHeightDef = self.view.frame.height - mapGap
+        locListHeightDef = self.view.frame.height - mapGapTop
         locList = Location.getAll() ?? [Location]()
         annotateLocationList(completion: { listDone in
             self.locList = Location.getAll() ?? [Location]()
             self.locTableView.reloadData()
         })
-        if selectedLoc.latitude == 0 {
-            setHeights()
-        }
     }
     
     func setHeights(){
         mapViewHeight = self.view.frame.height
-        mapListHeightConstraint.constant = mapViewHeight - mapGap
-        mapGapBottom = mapViewHeight - mapGap
+        mapListHeightConstraint.constant = mapViewHeight/2 - centreOffset
+        mapGapBottom = mapViewHeight - mapGapTop
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -276,7 +252,6 @@ extension LocViewController:  UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "locCell")!
         let loc: Location = locList[indexPath.section]
         cell.layer.cornerRadius = 30
@@ -295,10 +270,9 @@ extension LocViewController:  UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         let loc = locList[indexPath.section]
         selectedLoc = loc.toCL()
-        mapView.setCenter(loc.toCL(), animated: true)
+        mapView.setCenter(selectedLoc, animated: true)
         
         performSegue(withIdentifier: locationDetailSegue, sender: loc)
     }
